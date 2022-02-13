68236c7f91d6a6f29aab78493208a2f579a796bd
NoMios1234 <nomios1234@gmail.com>
2022-02-13 23:18:15 +0200

    @SW-6: add view models

---
+++ SmartWork/SmartWork.Core/ViewModels/UserViewModels/UserInfoViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SmartWork.Core.ViewModels.UserViewModels
+{
+    public class UserInfoViewModel
+    {
+        public string Id { get; set; }
+
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Full name")]
+        public string FullName { get; set; }
+
+        [Display(Name = "Phone number")]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        public DateTime DateOfBirth { get; set; }
+    }
+}
