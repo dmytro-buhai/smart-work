0fdba19f415eca5786059b3b684dea6fee04a2d0
NoMios1234 <nomios1234@gmail.com>
2022-02-18 18:41:32 +0200

    @SW-4: added role initializer

---
+++ SmartWork/SmartWork.Configuration/RoleInitializer.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SmartWork.Configuration.Resources;
+using SmartWork.Core.Entities;
+using System.Threading.Tasks;
+
+namespace SmartWork.Configuration
+{
+    public class RoleInitializer
+    {
+        public static async Task InitializeAsync(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            // Default admin account
+            var adminEmail = RoleInitializerResources.ResourceManager.GetString("AdminEmail");
+            var adminPassword = RoleInitializerResources.ResourceManager.GetString("AdminPassword");
+
+            // Roles
+            var adminRole = RoleInitializerResources.ResourceManager.GetString("AdminRoleName");
+            var userRole = RoleInitializerResources.ResourceManager.GetString("UserRoleName");
+
+            if ((await roleManager.FindByNameAsync(adminRole)) == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole(adminRole));
+            }  
+            if ((await roleManager.FindByNameAsync(userRole)) == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole(userRole));
+            }
+            if ((await userManager.FindByEmailAsync(adminEmail)) == null)
+            {
+                var admin = new User { Email = adminEmail, UserName = adminEmail };
+                var result = await userManager.CreateAsync(admin, adminPassword);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, adminRole);
+                }
+            }
+        }
+    }
+}
