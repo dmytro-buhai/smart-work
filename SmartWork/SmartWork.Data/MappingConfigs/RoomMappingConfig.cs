84683f302749297dc053e8ffdfd102cc4e07cdc2
NoMios1234 <nomios1234@gmail.com>
2022-02-19 21:59:14 +0200

    @SW-12: added data mapping configs

---
+++ SmartWork/SmartWork.Data/MappingConfigs/RoomMappingConfig.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SmartWork.Core.Entities;
+
+namespace SmartWork.Data.MappingConfigs
+{
+    public class RoomMappingConfig : IEntityTypeConfiguration<Room>
+    {
+        public void Configure(EntityTypeBuilder<Room> builder)
+        {
+            builder.ToTable("Rooms");
+
+            builder.Property(x => x.OfficeId)
+                .IsRequired(true);
+            builder.Property(x => x.Name)
+                .HasMaxLength(256)
+                .IsRequired(true);
+            builder.Property(x => x.Number)
+                .HasMaxLength(256)
+                .IsRequired(false);
+            builder.Property(x => x.Square)
+                .HasMaxLength(256)
+                .IsRequired(true);
+            builder.Property(x => x.PhotoFileName)
+                .IsRequired(true);            
+        }
+    }
+}
