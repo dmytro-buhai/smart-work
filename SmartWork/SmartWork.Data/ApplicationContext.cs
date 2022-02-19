8bd93f8d3a5899b70882b8cf1cb32de812c0e911
NoMios1234 <nomios1234@gmail.com>
2022-02-19 21:34:03 +0200

    @SW-8: added application context

--- SmartWork/SmartWork.Data/ApplicationContext.cs
+++ SmartWork/SmartWork.Data/ApplicationContext.cs
@@ -1,8 +1,19 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SmartWork.Core.Entities;
 
 namespace SmartWork.Data
 {
-    public class ApplicationContext
+    public class ApplicationContext : IdentityDbContext<User>
     {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+        public DbSet<Company> Company { get; set; }
+        public DbSet<Office> Office { get; set; }
+        public DbSet<Room> Room { get; set; }
+        public DbSet<Equipment> Equipment { get; set; }
+        public DbSet<Subscribe> Subscribe { get; set; }
+        public DbSet<SubscribeDetail> SubscribeDetail { get; set; }
+        public DbSet<Statistic> Statistic { get; set; }
     }
 }
