1b2f6bc3df17268bbbb85f60221574ae2f22e93b
NoMios1234 <nomios1234@gmail.com>
2022-03-25 17:04:53 +0200

    @SW-22: refactored services abstractions

--- SmartWork/SmartWork.Core/Abstractions/Services/IEntityService.cs
+++ SmartWork/SmartWork.Core/Abstractions/Services/IEntityService.cs
@@ -1,8 +1,21 @@
-﻿using SmartWork.Core.Entities;
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartWork.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace SmartWork.Core.Abstractions.Services
 {
-    public interface IEntityService : IService<Entity>
+    public interface IEntityService<TEntity> where TEntity : Entity
     {
+        Task<TEntity> FindAsync(int id);
+        Task<TEntity> FindAsync(Func<TEntity, bool> expression);
+        Task<IEnumerable<TEntity>> GetAsync(Func<TEntity, bool> expression);
+        Task<IActionResult> AddAsync(TEntity entity);
+        Task<IActionResult> AddAsync(IEnumerable<TEntity> entities);
+        Task<IActionResult> UpdateAsync(TEntity entity);
+        Task<IActionResult> UpdateAsync(IEnumerable<TEntity> entities);
+        Task<IActionResult> RemoveAsync(int id);
+        Task<IActionResult> RemoveAsync(IEnumerable<TEntity> entities);
     }
 }
