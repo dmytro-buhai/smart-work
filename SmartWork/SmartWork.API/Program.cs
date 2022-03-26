c096721630a248891a4b1e18f0ba8819aac0d6b2
NoMios1234 <nomios1234@gmail.com>
2022-03-27 00:05:29 +0200

    @SW-32: added api project

---
+++ SmartWork/SmartWork.API/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using SmartWork.Configuration;
+using System.Threading.Tasks;
+
+namespace SmartWork.API
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await HostSettings<Startup>.SetBuildSettings(CreateHostBuilder(args));
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
