3e11025f5521bfcd60d907c4e0122db8ec37f274
NoMios1234 <nomios1234@gmail.com>
2022-03-27 00:05:02 +0200

    @SW-31: added dependency resolver

---
+++ SmartWork/SmartWork.Configuration/DependencyResolver.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SmartWork.BLL.Services;
+using SmartWork.Core.Abstractions.Repositories;
+using SmartWork.Core.Abstractions.Services.Base;
+using SmartWork.Core.Entities;
+using SmartWork.Data;
+using SmartWork.Data.Repositories;
+
+namespace SmartWork.Configuration
+{
+    public class DependencyResolver
+    {
+        public IConfiguration Configuration { get; }
+
+        public DependencyResolver(IServiceCollection services)
+        {
+            Configuration = ConfigurationSettings.GetConfiguration();
+            ConfigureServices(services);
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            //services.Configure<ApplicationSettings>(Configuration.GetSection("ApplicationSettings"));
+
+            // Register DbContext class
+            services.AddDbContext<ApplicationContext>(options =>
+                 options.UseSqlServer(DBSettings.GetDBConnectionString()));
+
+            // Register Repositories
+            services.AddScoped<IUserRepository<User>, EFCoreUserRepository<User>>();
+            services.AddScoped<IEntityRepository<Company>, EFCoreRepository<Company>>();
+
+            // Register Services
+            services.AddTransient<ICompanyService, CompanyService>();
+        }
+    }
+}
